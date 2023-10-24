d4dddd092004e7991ceaeb385ba2c7bccba6e624
Georgie-x <Georgiex@gmail.com>
2023-10-24 12:03:57 +0100

    retry

--- src/components/AllArticles.jsx
+++ src/components/AllArticles.jsx
@@ -1,39 +1,38 @@
-import { useState, useEffect } from "react"
-import axios from "axios"
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function AllArticles(){
+function AllArticles() {
+  const [articles, setArticles] = useState([]);
 
-const [articles, setArticles] = useState([])
-
-useEffect(()=>{
-    axios.get("https://georgiex-news.onrender.com/api/articles").then(({data}) => {
+  useEffect(() => {
+    axios
+      .get("https://georgiex-news.onrender.com/api/articles")
+      .then(({ data }) => {
         const articleList = data.articles;
-        setArticles(articleList)
-        console.log(articleList)
-    })
-},[])
+        setArticles(articleList);
+        console.log(articleList);
+      });
+  }, []);
 
-
-
-    return(
-        <>
-        <h2>Latest articles</h2>
-        <ul>
-            {articles.map((article => {
-                return(<li className="all-articles" key={article.article_id}>
-                    <img src={article.article_img_url}/>    
-                    <p>#{article.article_id} {article.title} by {article.author} {article.created_at}</p>
-                    <hr></hr>
-
-
-                </li>)
-            }))}
-        </ul>
-        </>
-    )
+  return (
+    <>
+      <h2>Latest articles</h2>
+      <ul>
+        {articles.map((article) => {
+          return (
+            <li className="all-articles" key={article.article_id}>
+              <img src={article.article_img_url} />
+              <p>
+                #{article.article_id} {article.title} by {article.author}{" "}
+                {article.created_at}
+              </p>
+              <hr></hr>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
 
-
-
-
-export default AllArticles
+export default AllArticles;
